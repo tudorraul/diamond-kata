b5e1ea3e7a16eb5d8aab75b951b1315028625995
raultudor <tudorraul@gmail.com>
2025-08-15 17:09:41 +0300

    Red-Green-Refactor #1 - 'A' should print just "A"

--- src/main/java/kata/diamond/DiamondPrinter.java
+++ src/main/java/kata/diamond/DiamondPrinter.java
@@ -1,4 +1,10 @@
 package kata.diamond;
 
+import java.util.List;
+
 public class DiamondPrinter {
+    public List<String> printDiamond(char letter) {
+        if (letter == 'A') return List.of("A");
+        return List.of();
+    }
 }
--- src/test/java/kata/diamond/DiamondPrinterTest.java
+++ src/test/java/kata/diamond/DiamondPrinterTest.java
@@ -1,7 +1,18 @@
 package kata.diamond;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiamondPrinterTest {
+
+    @Test
+    void printsDiamondWithA() {
+        var printer = new DiamondPrinter();
+        var expected = List.of("A");
+        assertEquals(expected, printer.printDiamond('A'));
+    }
 
 }
